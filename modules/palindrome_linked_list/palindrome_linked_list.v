67e235b10f97e38790c047c297eebdf7cf71ad8c
baayso <baayso@outlook.com>
2022-04-02 01:33:40 +0800

    增加[判断该链表是否为回文链表]。

---
+++ modules/palindrome_linked_list/palindrome_linked_list.v
@@ -0,0 +1,49 @@
+
+// https://leetcode-cn.com/problems/palindrome-linked-list/
+// 回文链表
+
+[heap]
+struct ListNode  {
+	val  int
+mut:
+	next &ListNode
+}
+
+fn is_palindrome_linked_list(head &ListNode) bool {
+	mut list := []int{}
+
+	mut p := head
+	for p != 0 {
+		list << p.val
+		p = p.next
+	}
+
+	println('list = ${list}')
+
+	len := list.len
+
+	for i, j := 0, len - 1; i < j; i++, j-- {
+		if list[i] != list[j] {
+			return false
+		}
+	}
+
+	return true
+}
+
+fn main() {
+	head := &ListNode{val : 1, next: 0}
+	mut p := head
+
+	for i in 2 .. 5 {
+		p.next = &ListNode{val : i, next : 0}
+		p = p.next
+	}
+
+	for i := 4; i >= 1; i-- {
+		p.next = &ListNode{val : i, next : 0}
+		p = p.next
+	}
+
+	println(is_palindrome_linked_list(head))
+}
